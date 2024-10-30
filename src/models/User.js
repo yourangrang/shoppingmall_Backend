1bb225dd39e4266a18ad9fc16709e2345b38f24d
yourangrang <163999418+yourangrang@users.noreply.github.com>
2024-10-30 15:11:48 +0900

    로그인route생성, JWT설정

--- src/models/User.js
+++ src/models/User.js
@@ -1,6 +1,5 @@
-const bcrypt = require('bcryptjs');
-
 const { default: mongoose } = require("mongoose");
+const bcrypt = require('bcryptjs');
 
 const userSchema = mongoose.Schema({
     name: {
@@ -35,6 +34,13 @@ userSchema.pre('save', async function (next){
 
     next();
 })
+
+userSchema.methods.comparePassword = async function (plainPassword) {
+    let user = this;
+    console.log(user);
+    const match = await bcrypt.compare(plainPassword, user.password);
+    return match;
+}
 
 
 const User = mongoose.model("User", userSchema);
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,12 +1,42 @@
 const express = require('express');
 const User = require('../models/User');
 const router = express.Router();
+const jwt = require('jsonwebtoken');
 
 router.post('/register', async (req, res, next) =>{
     try {
         const user = new User(req.body);
         await user.save();
         return res.sendStatus(200);
+    } catch (error) {
+        next(error)
+    }
+})
+
+router.post('/login', async (req, res, next) =>{
+    try {
+       //존재하는 유저인지 체크
+       const user = await User.findOne({ email: req.body.email});
+
+       if(!user) {
+          return res.status(400).send("Auth failed, email not found");
+       }
+
+       // 비밀번호가 올바른 것인지 체크
+       const isMach = await user.comparePassword(req.body.password);
+       if(!isMach) {
+          return res.status(400).send('Wrong password');
+       }
+
+       const payload = {
+          userId: user._id.toHexString(),
+       }
+ 
+       // token생성
+       const accessToken = jwt.sign(payload, process.env.JWT_SECRET, {expiresIn: '1h' })
+
+       return res.json({ user, accessToken });
+
     } catch (error) {
         next(error)
     }
