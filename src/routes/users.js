16a7997d0fd7121b667c36f99e5a511f6200ebd7
yourangrang <163999418+yourangrang@users.noreply.github.com>
2024-11-01 17:50:59 +0900

    로그아웃 router 생성

--- src/routes/users.js
+++ src/routes/users.js
@@ -56,4 +56,13 @@ router.post('/login', async (req, res, next) =>{
     }
 })
 
+
+router.post('/logout', auth, async (req, res, next) =>{
+    try {
+        return res.sendStatus(200);
+    } catch (error) {
+        next(error)
+    }
+})
+
 module.exports = router
