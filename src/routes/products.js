439478e4ef7a49b2acb3e91e2d3231ebd57020a3
yourangrang <163999418+yourangrang@users.noreply.github.com>
2024-11-11 18:35:23 +0900

    RadioBox filters기능 추가

--- src/routes/products.js
+++ src/routes/products.js
@@ -34,7 +34,14 @@ router.get('/', async (req, res, next) => {
     let findArgs = {};
     for (let key in req.query.filters) {
         if (req.query.filters[key].length > 0) {
-            findArgs[key] = req.query.filters[key];
+            if (key === 'price') {
+                findArgs[key] = {
+                    $gte: req.query.filters[key][0],
+                    $lte: req.query.filters[key][1]
+                }
+            } else {
+                findArgs[key] = req.query.filters[key];
+            }
         }
     }
 
