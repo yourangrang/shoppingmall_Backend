c0856ecf4d23a11146b2782eed38ee5d70efc87b
yourangrang <163999418+yourangrang@users.noreply.github.com>
2024-11-12 17:33:20 +0900

    검색기능 생성

--- src/models/Product.js
+++ src/models/Product.js
@@ -40,6 +40,17 @@ const productSchema = mongoose.Schema({
     },
 })
 
+productSchema.index({
+    title: 'text',
+    description: 'text'
+}, {
+    weights: {
+        title: 5,
+        description: 1
+    }
+})
+
+
 const Product = mongoose.model("Product", productSchema);
 
 module.exports = Product;
